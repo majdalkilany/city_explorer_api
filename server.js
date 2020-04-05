53e3fe866e8697d941070942b9e3dbcc54a3a685
majdalkilany <majdkilany922@gmail.com>
2020-04-05 22:04:06 +0300

    lab-06

--- server.js
+++ server.js
@@ -17,15 +17,57 @@ server.listen(PORT,()=>{
     
 })
 
+
+
 server.get('/',(request,response)=>{ 
     response.status(200).send('done')
     
 })
-server.use('*',(request,response)=>{
-    response.status(404).send('PAGE NOT FOUND')
-});
-server.use((error,request,response)=>{
-    response.status(500).send(error);
+
+// localhost:3000/location?city=Lynnwood
+server.get('/location', (req, res) => {
+    const geoData = require('./data/geo.json');
+    const city = req.query.city;
+    const locationData = new Location(city,geoData);
+    res.send(locationData);
+
 })
 
-// localhost:/3000/location?city=Lynwood
+function Location (city,geoData) {
+    this.search_query = city;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
+}
+
+// localhost:3000/weather
+
+server.get('/weather', (req, res) => {
+    const weatherFile = require('./data/weather.json');
+    let arrOfwearthe=[];
+    weatherFile.data.forEach((val, i) => {
+        let description = val.weather.description;
+        let date = val.valid_date;
+        let majd = new weatherData(description, date);
+        arrOfwearthe.push(majd);
+
+    })
+    res.send(arrOfwearthe);
+    
+
+})
+
+function weatherData(description, date) {
+    this.forecast = description;
+    this.time=date;
+
+}
+// localhost:3000/anything
+server.use('*', (req, res) => {
+    res.status(404).send('NOT FOUND');
+});
+
+server.use((error, req, res) => {
+    res.status(500).send(error);
+})
+
